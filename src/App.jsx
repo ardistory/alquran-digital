9711f2e78e23431f530a693def9885033f368d0b
Ardiansyah Putra <putraardiansyah530@gmail.com>
2025-07-13 12:46:45 +0700

    Update: check ayat

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,10 @@ function App() {
     const [allSurat, setAllSurat] = useState([]);
     const [qurans, setQurans] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-
+    const [ayatChecked, setAyatChecked] = useState(() => {
+        const storedAyatChecked = localStorage.getItem('ayatChecked');
+        return storedAyatChecked ? JSON.parse(storedAyatChecked) : [];
+    });
 
     const getQuranFromApi = async (nomor = 1) => {
         const { data } = await axios.get(`${import.meta.env.VITE_API_URL_SURAT}/${nomor}`);
@@ -24,10 +27,13 @@ function App() {
     };
 
     useEffect(() => {
+        localStorage.setItem('ayatChecked', JSON.stringify(ayatChecked));
+    }, [ayatChecked]);
+
+    useEffect(() => {
         const getAllSurat = async () => {
             const { data } = await axios.get(`${import.meta.env.VITE_API_URL_SURAT}`);
             setAllSurat(data.data);
-            (Object.keys(data.data).length > 0) ? setIsLoading(false) : setIsLoading(true);
         };
 
         getAllSurat();
@@ -38,18 +44,56 @@ function App() {
         getQuranFromApi(nomor);
     };
 
+    const checkAyat = (namaSurat, nomorAyat) => {
+        const existingSurat = ayatChecked.find(surat => surat.namaSurat === namaSurat);
+        if (existingSurat) {
+            const updatedAyatChecked = ayatChecked.map(surat => {
+                if (surat.namaSurat === namaSurat) {
+                    return { ...surat, ayat: [...surat.ayat, nomorAyat] };
+                }
+                return surat;
+            });
+            setAyatChecked(updatedAyatChecked);
+        } else {
+            setAyatChecked(prev => [...prev, { namaSurat, ayat: [nomorAyat] }]);
+        }
+    };
+
+    const unCheckAyat = (namaSurat, nomorAyat) => {
+        const existingSurat = ayatChecked.find(surat => surat.namaSurat === namaSurat);
+        if (existingSurat) {
+            const updatedAyatChecked = ayatChecked.map(surat => {
+                if (surat.namaSurat === namaSurat) {
+                    return { ...surat, ayat: surat.ayat.filter(ayat => ayat !== nomorAyat) };
+                }
+                return surat;
+            }).filter(surat => surat.ayat.length > 0);
+            setAyatChecked(updatedAyatChecked);
+        }
+    };
+
+    const validasiCheckAyat = (namaSurat, nomorAyat) => {
+        const ayatFind = ayatChecked.find(ayat => ayat.namaSurat === namaSurat);
+
+        if (ayatFind?.ayat.includes(nomorAyat)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
     const clearData = () => {
     };
 
     return (
         <AppLayout>
             <div className={'space-y-5'}>
-                <div className={'sticky top-28'}>
+                <div className={'sticky top-28 z-10'}>
                     <Card>
                         <CardHeader>
                             <CardTitle>
                                 <div className={'flex flex-col gap-5'}>
-                                    <Select onValueChange={(value) => getQuranFromApi(value)}>
+                                    <Select defaultValue={1} onValueChange={(value) => getQuranFromApi(value)}>
                                         <SelectTrigger>
                                             <SelectValue placeholder={'Cari Surat...'} />
                                         </SelectTrigger>
@@ -106,11 +150,11 @@ function App() {
                                 <div className={'fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]'}>
                                     <Loader className={'animate-spin'} size={55} />
                                 </div>
-                            ) : qurans.ayat.map((quran, index) => (
-                                <Card key={index} className={'space-y-5 ring ring-white/10'}>
+                            ) : qurans?.ayat.map((quran, index) => (
+                                <Card key={index} className={'space-y-5 relative'}>
                                     <CardHeader>
                                         <CardTitle className={'font-Amiri text-2xl text-end leading-[55px]'}>
-                                            {quran.teksArab}({convertToArabIndia(quran.nomorAyat)})
+                                            {(validasiCheckAyat(qurans.namaLatin, quran.nomorAyat)) ? (<div className={'before:w-10 before:h-10 before:bg-yellow-300/50 before:rounded-br-[200px] before:block before:rounded-tl-[55px] absolute left-0 top-0'} />) : null}{quran.teksArab}({convertToArabIndia(quran.nomorAyat)})
                                         </CardTitle>
                                         <CardDescription className={'text-xs'}>
                                             {quran.teksIndonesia}
@@ -126,10 +170,15 @@ function App() {
                                                 <BookOpenCheck />
                                                 Tafsir
                                             </Button>
-                                            <Button>
-                                                <CheckCheck />
-                                                Tandai
-                                            </Button>
+                                            {(validasiCheckAyat(qurans.namaLatin, quran.nomorAyat)) ? (
+                                                <Button size={'icon'} onClick={() => unCheckAyat(qurans.namaLatin, quran.nomorAyat)}>
+                                                    <X />
+                                                </Button>
+                                            ) : (
+                                                <Button size={'icon'} onClick={() => checkAyat(qurans.namaLatin, quran.nomorAyat)}>
+                                                    <CheckCheck />
+                                                </Button>
+                                            )}
                                         </div>
                                     </CardContent>
                                 </Card>
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -3,7 +3,7 @@ import ToggleTheme from "@/components/ToggleTheme.jsx";
 const AppLayout = ({ children }) => {
     return (
         <div className={'font-Inter'}>
-            <div className={'bg-white dark:bg-black dark:text-white w-full h-24 flex items-center border-b-2 fixed'}>
+            <div className={'bg-white dark:bg-black dark:text-white w-full h-24 flex items-center border-b-2 fixed z-10'}>
                 <div className={'container mx-auto px-5 md:px-0 flex items-center justify-between'}>
                     <div className={'text-4xl italic font-bold'}>
                         Qur'an Digital
